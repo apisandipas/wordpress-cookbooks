f5f96e944d4d43fb0e0729e13652e796daae2145
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-12-18 20:17:02 -0600

    Merge imagemagick and libmagic packages into one

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -1,9 +1,10 @@
 package :imagemagick do
   description "ImageMagick image processing library"
 
-  apt "imagemagick"
+  apt "imagemagick", "libmagick++-dev"
 
   verify do
     has_apt "imagemagick"
+    has_apt "libmagick++-dev"
   end
 end
