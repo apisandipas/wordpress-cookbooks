44e9cd11f65373ac80cb90b1e18a72ed8b6580d4
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-05 22:24:12 -0600

    Remove packages from config into build policy

--- build.rb
+++ build.rb
@@ -7,7 +7,32 @@ $config = Hashr.new(YAML.load_file("./config.yml"))
 Dir["./packages/**/*.rb"].each { |f| require(f) } 
 
 policy :build, :roles => :app do
-  $config.packages.each { |name| requires(name) }
+  requires :apt_update
+  requires :apt_upgrade
+  requires :system_tools
+  requires :ppa
+  requires :sudo
+  requires :locales
+  requires :ufw
+  requires :ufw_rules
+  requires :openssh_configs
+  requires :ntpdate
+  requires :build_essentials
+  requires :git
+  requires :subversion
+  requires :imagemagick
+  requires :mysql
+  requires :mysql_client
+  requires :mysql_setup
+  requires :add_user
+  requires :setup_user
+  requires :nginx
+  requires :php5
+  requires :php5_extensions
+  requires :php5_fpm
+  requires :wordpress
+  requires :wordpress_vhost
+  requires :clean_logs
 end
 
 deployment do
